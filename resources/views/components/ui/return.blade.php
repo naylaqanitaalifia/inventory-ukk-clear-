a9d06bdf84b484fa43d6e2bd047f733441fed904
naylaqanitaalifia <naylaqanita286@gmail.com>
2026-04-10 22:54:22 +0700

    first commit

---
+++ resources/views/components/ui/return.blade.php
@@ -0,0 +1,9 @@
+<td class="px-1 py-1 whitespace-nowrap">
+    <div class="px-3 py-1 text-sm font-medium text-center text-green-400 dark:text-white border border-green-400 rounded-full inline-block">
+        {{ $detail->return_date->format('j F, Y') }}
+    </div>
+    {{-- batas --}}
+    <div class="px-3 py-1 text-sm font-medium text-center text-amber-400 dark:text-white border border-amber-400 rounded-full inline-block">
+        Not Returned
+    </div>
+</td>
