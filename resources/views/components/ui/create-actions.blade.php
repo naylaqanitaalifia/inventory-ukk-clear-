6e4f5b5c285723064e0452b9b582183d09fdbbeb
naylaqanitaalifia <naylaqanita286@gmail.com>
2026-04-11 03:54:35 +0700

    first commit

---
+++ resources/views/components/ui/create-actions.blade.php
@@ -0,0 +1,8 @@
+<div class="flex justify-end gap-3 pt-4 border-t border-gray-100 dark:border-gray-800">
+    <a href="#" class="flex items-center justify-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-theme-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200">
+        Cancel
+    </a>
+    <button type="submit" class="flex items-center justify-center rounded-lg bg-brand-500 px-4 py-2 text-sm font-medium text-white hover:bg-brand-600 transition">
+        Save Item
+    </button>
+</div>
